9e9ca751f3c8545ab90fb4d3884a1af196533187
Igor <route616@gmail.com>
2021-03-28 17:19:44 +0300

    Initial

--- TaskManager/ContentView.swift
+++ TaskManager/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView().preferredColorScheme(.light).previewDevice("iPhone 12 mini")
+        }
     }
 }
